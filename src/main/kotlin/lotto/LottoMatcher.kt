829f2d5bb6f02b49d814aa452ff602f1f1e78070
cbnuLeehyunwoo <henow123@naver.com>
2025-11-02 03:10:25 +0900

    feat(LottoMatcher.kt): 로또 당첨 결과를 판별하는 LottoMatcher 클래스 구현
    로또 당첨 결과를 판별하는 클래스를 구현하였습니다 해당 클래스 내에
    사용자 로또 번호와 당첨 번호를 비교하여 등수를 계산하는 calculateRank
    함수를 구현하였습니다.

---
+++ src/main/kotlin/lotto/LottoMatcher.kt
@@ -0,0 +1,19 @@
+package lotto
+
+class LottoMatcher(
+    private val numbers: List<Int>,
+    private val bonusNumber: Int
+) {
+    fun calculateRank(userLotto: Lotto): Rank {
+        val matchCount = userLotto.matchNumberCount(numbers)
+        val bonusMatched = userLotto.hasNumber(bonusNumber)
+
+        return when(matchCount) {
+            6 -> Rank.FIRST
+            5 -> if(bonusMatched) Rank.SECOND else Rank.THIRD
+            4 -> Rank.FOURTH
+            3 -> Rank.FIFTH
+            else -> Rank.MISS
+        }
+    }
+}
