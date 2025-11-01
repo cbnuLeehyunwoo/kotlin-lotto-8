c7c58d90b2761427e26d58f84ebf80c22f8f8f4f
cbnuLeehyunwoo <henow123@naver.com>
2025-11-01 12:36:26 +0900

    feat(Application.kt): 로또 구입 금액을 입력받는 기능 구현
    로또 구입 금액을 입력받는 함수 getLottoPurchaseAmount 함수를
    구현했습니다.

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,13 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console.readLine
+
 fun main() {
     // TODO: 프로그램 구현
+    val lottoPurchaseAmount = getLottoPurchaseAmount()
+}
+
+fun getLottoPurchaseAmount(): Int {
+    println("구입금액을 입력해 주세요.")
+    return readLine().toInt()
 }
