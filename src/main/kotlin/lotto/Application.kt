fc7443479f7f55dab0baede98951359594c80559
cbnuLeehyunwoo <henow123@naver.com>
2025-11-01 12:50:20 +0900

    feat(Application.kt): 구매할 로또 개수를 출력하는 기능 구현
    구매할 로또 개수를 구하는 calculateLotttoCount 함수를 구현했습니다.

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -5,9 +5,15 @@ import camp.nextstep.edu.missionutils.Console.readLine
 fun main() {
     // TODO: 프로그램 구현
     val lottoPurchaseAmount = getLottoPurchaseAmount()
+    val lottoCount = calculateLottoCount(lottoPurchaseAmount)
+    println("\n${lottoCount}개를 구매했습니다.")
 }
 
 fun getLottoPurchaseAmount(): Int {
     println("구입금액을 입력해 주세요.")
     return readLine().toInt()
+}
+
+fun calculateLottoCount(amount: Int): Int {
+    return amount / 1000
 }
